25e068492b0fc9bff29f5ef5051a466ae25dfa4a
RAJAPPA Naveen <naveen.rajappa@aldautomotive.com>
2018-12-28 14:54:51 +0100

    Add project files.

---
+++ FleckMerchandiser-2018/Data/FleckMerchandiserContext.cs
@@ -0,0 +1,31 @@
+﻿using FleckMerchandiser2018.Models.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FleckMerchandiser2018.Data
+{
+    public class FleckMerchandiserContext : DbContext
+{
+        private readonly string connectionStringName;
+
+        public FleckMerchandiserContext(DbContextOptions<FleckMerchandiserContext> options) : base(options)
+        {
+
+        }
+
+        public FleckMerchandiserContext(string ConnectionStringName)
+        {
+            this.connectionStringName = ConnectionStringName;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().ToTable("Users");
+        }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ FleckMerchandiser-2018/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FleckMerchandiser_2018.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
